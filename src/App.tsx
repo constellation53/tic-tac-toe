ca6daf8161befa6f153593faf4d89071bc6ab2d5
constellation53 <almahanov567@gmail.com>
2023-07-15 19:25:43 +0800

    lint: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FC, ReactElement, useState } from 'react';
+import reactLogo from './assets/react.svg';
+// eslint-disable-next-line import/no-unresolved, import/no-absolute-path
+import viteLogo from '/vite.svg';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const Square = (): ReactElement => <button className="square">X</button>;
+
+type PropsType = {
+  children?: never;
+};
+
+const App: FC<PropsType> = (): ReactElement => {
+  const [count, setCount] = useState(0);
+
+  const onClick = (): void => setCount((value) => value + 1);
 
   return (
     <>
@@ -18,7 +27,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={onClick}>
           count is {count}
         </button>
         <p>
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
