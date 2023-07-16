d9c2679c1e93eacfab7ae94ad21d16603af7f071
constellation53 <almahanov567@gmail.com>
2023-07-16 22:20:46 +0800

    feat: add handler to each square

--- src/modules/game/components/elements/square.tsx
+++ src/modules/game/components/elements/square.tsx
@@ -1,7 +1,15 @@
 // Core
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 // Other
 import { PropsType } from './types';
 
-export const Square: FC<PropsType> = ({ value }) => <button className="square">{value}</button>;
+export const Square: FC<PropsType> = () => {
+
+  const [value, setValue] = useState<string | null>(null);
+  const onClick = (): void => {
+    setValue('X');
+  };
+
+  return <button className="square" onClick={onClick}>{value}</button>;
+};
--- src/modules/game/components/elements/types/index.ts
+++ src/modules/game/components/elements/types/index.ts
@@ -1,4 +1,3 @@
 export type PropsType = {
-  value: number;
   children?: never;
 };
