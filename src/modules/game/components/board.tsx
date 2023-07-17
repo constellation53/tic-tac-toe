3122a1c0b9a92574efe5f2ec143e8984fd4d173c
constellation53 <almahanov567@gmail.com>
2023-07-17 21:11:52 +0800

    feat: add calculation

--- src/modules/game/components/board.tsx
+++ src/modules/game/components/board.tsx
@@ -3,32 +3,44 @@ import { FC, ReactElement, useState } from 'react';
 
 // Other
 import { PropsType } from './types';
-import { Square } from './elements/square.tsx';
+import { Index } from './elements/square';
+import { calculateWinner } from './utils';
 
 export const Board: FC<PropsType> = (): ReactElement => {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState<string[]>(Array(9).fill(null));
 
   const onSquareClick = (index: number): void => {
     const nextSquares = squares.slice();
-    nextSquares[index] = 'X';
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+
+    if (xIsNext) {
+      nextSquares[index] = 'X';
+    } else {
+      nextSquares[index] = 'O';
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   };
 
   return <div>
     <div className="board-row">
-      <Square value={squares[0]} onClick={onSquareClick}/>
-      <Square value={squares[1]}/>
-      <Square value={squares[2]}/>
+      <Index value={squares[0]} onClick={() => onSquareClick(0)}/>
+      <Index value={squares[1]} onClick={() => onSquareClick(1)}/>
+      <Index value={squares[2]} onClick={() => onSquareClick(2)}/>
     </div>
     <div className="board-row">
-      <Square value={squares[3]}/>
-      <Square value={squares[4]}/>
-      <Square value={squares[5]}/>
+      <Index value={squares[3]} onClick={() => onSquareClick(3)}/>
+      <Index value={squares[4]} onClick={() => onSquareClick(4)}/>
+      <Index value={squares[5]} onClick={() => onSquareClick(5)}/>
     </div>
     <div className="board-row">
-      <Square value={squares[6]}/>
-      <Square value={squares[7]}/>
-      <Square value={squares[8]}/>
+      <Index value={squares[6]} onClick={() => onSquareClick(6)}/>
+      <Index value={squares[7]} onClick={() => onSquareClick(7)}/>
+      <Index value={squares[8]} onClick={() => onSquareClick(8)}/>
     </div>
   </div>;
 };
--- src/modules/game/components/elements/square/index.tsx
+++ src/modules/game/components/elements/square/index.tsx
@@ -4,6 +4,6 @@ import { FC } from 'react';
 // Other
 import { PropsType } from './types';
 
-export const Square: FC<PropsType> = ({ value, onClick}) => {
+export const Index: FC<PropsType> = ({ value, onClick}) => {
   return <button className="square" onClick={onClick}>{value}</button>;
 };
